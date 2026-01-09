16cfee936c2ade66687eca0d0b6428a8b80e8ffb
M Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-09 18:57:44 +0500

    Feat: Added File For 1 and 0 Triangle

---
+++ Loops/Nested Loops/1 and 0 triangle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter a number : ";cin>>x;
+	
+	
+	for(int i=1; i<=x; i++)
+	{
+		
+		for(int j=1; j<=i; j++)
+		{
+			if ((i+j)%2==0)
+			{
+				cout<<" 1 ";
+			}
+			else
+			{
+				cout<<" 0 ";
+			}
+		}
+		cout<<endl;
+	}
+}
