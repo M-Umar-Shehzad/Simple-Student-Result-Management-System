fe14ca5b6b81ff520688e528b91c838a2c02ed60
M Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-13 21:47:49 +0500

    Feat: Added File For Triangle Flipped

---
+++ Loops/Nested Loops/Triangle Flipped.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter a number : ";cin>>x;
+	
+	for(int i=1;i<=x;i++)
+	{
+		for(int j=1;j<=x-i+1;j++)
+		{
+			cout<<j;
+		}
+		cout<<"\n";
+	}
+	
+return 0;
+}
