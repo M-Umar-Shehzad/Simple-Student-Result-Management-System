ff2707708125e1bbdaad305a2be2974885ebd960
M Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-13 21:49:14 +0500

    Feat: Added Code For Binary Triangle

---
+++ Loops/Nested Loops/Binary Triangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter a Numebr : ";cin>>x;
+	
+	for(int i=1; i<=x; i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			if((i+j)%2==0)
+			{
+				cout<<"1";
+			}
+			else
+			{
+				cout<<"0";
+			}
+		}
+		cout<<endl;
+	}
+return 0;
+}
