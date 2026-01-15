b88f1387415d11d283bb4359efc42367c9e5dd2a
M Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-15 21:51:54 +0500

    Feat: Added File For British Flag

---
+++ Loops/Nested Loops/Birtish flag.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter a number : ";cin>>x;
+	
+	for(int i=1;i<=x;i++)
+	{
+		for(int j=1;j<=x;j++)
+		{
+			if(i==(x/2)+1 or j == (x/2)+1 or i == j or i+j == x+1)
+			{
+				cout<<" * ";
+			}
+			else
+			{
+				cout<<"   ";
+			}
+		}
+		cout<<endl;
+	}
+	
+return 0;
+}
