f3c8d7100ed1ce9f30fa23526c98af22368fceb4
M Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-15 21:23:57 +0500

    Feat: Added File FOr Alphabet Pyramid Ascending and Descending

---
+++ Loops/Nested Loops/Alphabet_Pyramid_A_and_D.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter a Number : "; cin>>n;
+	
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=1; j<=n-i; j++)
+		{
+			cout<<" ";
+		}
+		for(int j=1; j<=i; j++)
+		{
+			cout<<(char)(j+64);
+		}
+		for(int j=1; j<=i-1; j++)
+		{
+			cout<<(char)((i-j)+64);
+		}
+		cout<<endl;
+	}
+	
+return 0;
+}
