8f03a47ba74adb44341c20fabd4a4aae0324887a
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-19 23:16:20 +0500

    Feat: Added file fore Flipped_Pyramid_2

---
+++ Loops/Nested Loops/Flipped_Pyramid_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"For triangle Enter A Number : ";cin>>x;
+	
+	for(int i=1; i<=x; i++)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			cout<<" ";
+		}
+		
+		for(int j=1; j<=(2*(x-i+1))-1;j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	
+	
+}
