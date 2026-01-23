25fba4d3df333371af64098d37bf40919dc04b1a
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-23 22:07:21 +0500

    Feat: Added file for Diamond 2

---
+++ Loops/Nested Loops/diamond_2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter A Number : ";cin>>x;
+	
+	for(int i=1; i<=x; i++)
+	{
+		for(int j=1; j<=x-i+1; j++)
+		{
+			cout<<" ";
+		}
+		for(int j=1;j<=2*i-1;j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	for(int i=2; i<=x; i++)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			cout<<" ";
+		}
+		for(int j=1;j<=2*(x-i)+1;j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+return 0;	
+}
