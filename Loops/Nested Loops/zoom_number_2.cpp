73cc0ece24a9d9daf15e5685310e1e85bbf0edb3
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-01-27 11:56:54 +0500

    Feat: Added file for Zoom Number 2

---
+++ Loops/Nested Loops/zoom_number_2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"ENter a Number : ";cin>>x;
+	int max=0;
+	
+	for (int i=1; i<=2*x-1; i++)
+	{
+		for(int j=1; j<=2*x-1; j++)
+		{
+			int a=i,b=j;
+			
+			if(i<x)
+			{
+				a=2*x-i;
+			}
+			if(j<x)
+			{
+				b=2*x-j;
+			}
+			if(a>b)
+			{
+				max=a;
+			}
+			else
+			{
+				max=b;
+			}
+			cout<<max<<" ";
+		}
+		cout<<endl;
+	}
+return 0;
+} 
