02b24a71e321de6dee6512f94f53e6df40d5fd5b
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-02-01 20:30:00 +0500

    Feat: Added file for Power + made a minor streak fix

---
+++ Functions/Power.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int pow(int x, int y)
+{
+	int a=1;
+	for(int i=1; i<=y; i++)
+	{
+		a*=x;
+	}
+	return a;
+}
+int main()
+{
+	int x,y;
+	cout<<"Enter a Number : ";cin>>x;
+	cout<<"Enter value of Exponent : "; cin>>y;
+	
+	int a=pow(x,y);
+	cout<<x<<" raise to the power "<<y<<" gives : "<<a;
+}minor formatting
