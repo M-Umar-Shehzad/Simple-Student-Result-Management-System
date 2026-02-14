cd34111192f9c53ac39cd4af650afed2b793d2ee
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-02-14 22:38:31 +0500

    Feat: Added file for Hollow Diamond

---
+++ Practice/HollowDiamond.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x;
+  cout << "Enter a number: ";
+  cin >> x;
+
+  for (int i = 1; i <= x; i++)
+  {
+
+    for (int j = 1; j <= x - i + 1; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = 1; j <= 2 * i - 1; j++)
+    {
+      if (j == 1 || j == 2 * i - 1)
+      {
+        cout << "*";
+      }
+      else
+      {
+        cout << " ";
+      }
+    }
+    cout<<endl;
+  }
+
+
+
+  for (int i = 2; i <= x; i++)
+  {
+      for (int j = 1; j <= i; j++)
+      {
+        cout << " ";
+      }
+
+      for (int j = 1; j <= (2 * (x - i + 1)) - 1; j++)
+      {
+        if (j == 1 || j == (2 * (x - i + 1)) - 1)
+        {
+          cout << "*";
+        }
+        else
+        {
+          cout << " ";
+        }
+      }
+    cout<<endl;
+  }
+ return 0;
+}
