350514aa762ac83a3fffe5827eb96ad92d8703dc
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-02-20 22:57:58 +0500

    Feat: Added file for Addition Trough Return-Type

---
+++ Recursion/Addition_through_return_type_in_recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sum(int x)
+{
+	if(x==1) return 1;
+	
+	return x + sum(x-1);
+}
+int main()
+{
+	int x,a;
+	cout<<"Enter a number : ";cin>>x;
+	a=sum(x);
+	cout<<a;
+	
+return 0;
+}
